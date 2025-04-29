b0ce0fc545a9871304294e0a2aca686e61fdece0
Oleksii Zenchenko <olekzenchenko@gmail.com>
2025-04-29 16:52:08 +0300

    Додали AutoMapper та моделі DTO для мапінгу

---
+++ Food_Delivery.BLL/Mapping/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using FoodDelivery.BLL.Models;
+using FoodDelivery.DAL.Entities;
+
+namespace FoodDelivery.BLL.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Маппінг з DAL до BLL
+            CreateMap<Category, CategoryDto>();
+            CreateMap<Dish, DishDto>();
+            CreateMap<DAL.Entities.DayOfWeek, DayOfWeekDto>();
+            CreateMap<Menu, MenuDto>();
+            CreateMap<Order, OrderDto>();
+            CreateMap<OrderItem, OrderItemDto>();
+
+            // Маппінг з BLL до DAL
+            CreateMap<CategoryDto, Category>();
+            CreateMap<DishDto, Dish>();
+            CreateMap<DayOfWeekDto, DAL.Entities.DayOfWeek>();
+            CreateMap<MenuDto, Menu>();
+            CreateMap<OrderDto, Order>();
+            CreateMap<OrderItemDto, OrderItem>();
+        }
+    }
+}
